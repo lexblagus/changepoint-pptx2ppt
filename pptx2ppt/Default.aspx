3c9716abef808f4f388c8be8fd448f447bb403a7
Lex Blagus <lex@blag.us>
2015-12-20 20:15:01 -0200

    Its just beautifull.

--- pptx2ppt/Default.aspx
+++ pptx2ppt/Default.aspx
@@ -1,9 +1,19 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="pptx2ppt._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-
-    <div class="jumbotron">
-        <h1>PPT to PPTX converter</h1>
+    <h1>PPT to PPTX converter</h1>
+    <p>Use local (at server) path to source and destination files using double backslashes (\\); exampli gratia in the textboxes bellow.<br /></p>
+    <input id="txbInput" name="input" placeholder="C:\\Windows\\Temp\\convert-this.pptx" style="width:100%;" onchange="javascript:translateURI();" />
+    <input id="txbOutput" name="output" placeholder="C:\\Windows\\Temp\\converted.pptx" style="width:100%;" onchange="javascript:translateURI();" />
+    <div style="text-align:right;">
+        <input type="submit" value="Convert" />
     </div>
-
+    <asp:Label ID="lblDebug" runat="server" Text="" Style="display:block; margin:1ex 0; padding:0.25em; font-family:monospace; white-space:pre; border:1px solid #808080; background-color:#FFFFCC; overflow:visible;"></asp:Label>
+    <script type="text/javascript">
+        self.translateURI = function () {
+            var compiledURI = location.protocol + '//' + location.host + location.pathname + '?input=' + document.getElementById('txbInput').value + '&output=' + document.getElementById('txbOutput').value;
+            var linkToURI = '<a href="'+compiledURI+'">'+compiledURI+'</a>';
+            document.getElementById("MainContent_lblDebug").innerHTML = 'URI to this convertion:' + '\n' + linkToURI;
+        }
+    </script>
 </asp:Content>
--- pptx2ppt/Default.aspx.cs
+++ pptx2ppt/Default.aspx.cs
@@ -6,8 +6,10 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 using System.IO;
-//using Aspose.Slides;
-//using Aspose.Slides.Export;
+using Aspose.Slides;
+using Aspose.Slides.Export;
+
+
 
 namespace pptx2ppt
 {
@@ -15,20 +17,50 @@ namespace pptx2ppt
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            lblDebug.Text = "";
+            if (IsPostBack || (Request.QueryString["input"] != null && Request.QueryString["output"] != null))
+            {
+                lblDebug.Text += "Initializing at " + DateTime.Now.ToLongTimeString() + "…" + "\n";
+                
+                // Set input parameters
+                string inputFile = Request.QueryString["input"];
+                string outputFile = Request.QueryString["output"];
+                lblDebug.Text += "Input=" + inputFile + "\n";
+                lblDebug.Text += "Output=" + outputFile + "\n";
+                
+                if (inputFile != null && outputFile != null && inputFile != "" && outputFile != "")
+                {
+                    if (File.Exists(inputFile)) {
 
-        }
-        public static void Run()
-        {
-            // The path to the documents directory.
-            //string dataDir = RunExamples.GetDataDir_Presentations();
+                        // Manual or debug
+                        //string inputFile = "C:\\Users\\Administrator\\Gallery\\Work\\Ornitorrinko\\PDTI-Sebrae\\pptx2ppt\\publish\\input-files\\convert-this.pptx";
+                        //string outputFile = "C:\\Users\\Administrator\\Gallery\\Work\\Ornitorrinko\\PDTI-Sebrae\\pptx2ppt\\publish\\output-files\\converted.ppt";
 
-            //Instantiate a Presentation object that represents a PPTX file
-            //Presentation pres = new Presentation(dataDir + "PPTtoPPTX.ppt");
+                        lblDebug.Text += "Converting…" + "\n";
+                        Response.Flush();
 
-            //Saving the PPTX presentation to PPTX format
-            //pres.Save(dataDir + "PPTtoPPTX.pptx", SaveFormat.Pptx);
+                        //Instantiate a Presentation object that represents a PPTX file
+                        Presentation pres = new Presentation(inputFile);
 
+                        //Saving the PPTX presentation to PPT format
+                        pres.Save(outputFile, SaveFormat.Ppt);
 
+                        lblDebug.Text += "Converted." + "\n";
+                        Response.Flush();
+                    }
+                    else
+                    {
+                        lblDebug.Text += "Error: input file does not exists." + "\n";
+                    }
+                }
+                else
+                {
+                    lblDebug.Text += "No files selected." + "\n";
+                }
+                lblDebug.Text += "Finished!" + "\n";
+            }else{
+                lblDebug.Text += "Converter at your service." + "\n";
+            }
         }
     }
 }
